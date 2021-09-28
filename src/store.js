4bffb985c5feafd07c69a3ed8a5e2095fc79346c
[2기-2반] 임희지 <limhizy15@ajou.ac.kr>
2021-09-29 01:10:52 +0900

    add: redux store for count data

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const incrementCount = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrementCount = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
